cb8901948dc1ce66a1fcb3c317f8a48c606c6eaf
Anusreepreetharanjith <89023599+Anusreepreetharanjith@users.noreply.github.com>
2021-08-18 11:07:06 +0530

    Delete app.js
    nthinh

--- app.js
+++
@@ -1,9 +0,0 @@
-let num = document.getElementById("number");
-var output = '';
-function addlist(){
-    if(num.value !=''){
-
-        output += `<li> ${num.value} </li> `;
-    }
-    document.getElementById("numberList").innerHTML = output;
-}
